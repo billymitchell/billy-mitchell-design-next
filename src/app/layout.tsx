6f10ea8038911cb96442216b2719745e7a61ab40
Billy Mitchell <hello@billymitchell.design>
2024-11-08 12:56:23 -0500

    remove global state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import '../styles/global.scss';
 import Script from 'next/script';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-import { GlobalStateProvider } from './context/setGlobalState';
 
 export default function RootLayout({
   children,
@@ -22,15 +21,13 @@ export default function RootLayout({
         <Script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js" />
         <Script src="https://kit.fontawesome.com/da815149ed.js" />
 
-        <GlobalStateProvider>
-          <div className="outer-wrapper">
-            <div className="main-container">
-              <Header />
-              {children}
-            </div>
+        <div className="outer-wrapper">
+          <div className="main-container">
+            <Header />
+            {children}
           </div>
-          <Footer />
-        </GlobalStateProvider>
+        </div>
+        <Footer />
       </body>
     </html>
   );
