388cef05b7ee6347b40e8bfa410ced548038521d
Billy Mitchell <hello@billymitchell.design>
2024-12-08 17:54:43 -0500

    metadata edits

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,31 @@ import Companies from '../components/home/Companies';
 import FetchCompanies from '../components/home/getCompanies';
 import ParallaxProviderWrapper from '../components/utilities/ParallaxProviderWrapper';
 
+// to do:
+
+// IMPORTANT
+// get animation delays working
+// get specialty animation bars working
+// update resume document
+// rework metadata and seo
+// refactor and simplify code
+// add/remove featured portfolio content
+// move sub domains to vercel
+
+// NICE TO HAVE
+// evaluate use of 3rd party image hosting
+// get contact section form working
+// add portfolio all page
+// add 404 page
+// add favicon
+// add robots.txt
+// add sitemap.xml
+// add analytics
+// add gtag
+// add favicon
+// add robots.txt
+// add sitemap.xml
+
 export default function Home() {
   return (
     <ParallaxProviderWrapper>
